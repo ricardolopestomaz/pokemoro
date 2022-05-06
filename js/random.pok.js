affd23c952771b214724f04c9841238b376805ac
Ricardo Lopes <ricardolptz42@mail.com>
2022-05-06 18:15:24 -0300

    Random pokemon V2

--- js/random.pok.js
+++ js/random.pok.js
@@ -1,20 +1,43 @@
-
-//RANDOM POKEBOLA
+//Rare
+const rare = [3,6,9,26,31,34,38,59,65,68,94,115,130,131,132,137,139,141,142,143,149]
+//Legendary
+const legendary = [144,145,146,150,151]
 
 // RANDOM POKEMON
-var id = Math.floor(Math.random() * 150) + 1
+let id = Math.floor(Math.random() * 151) + 1
 // PULL POKEMON
-const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 
-    fetch(url)
-    .then(response => response.json())
-    .then(pokemon => {
-        console.log("Nome: "+pokemon.name+"\nId: "+pokemon.id+"\nimg: "+ pokemon.sprites.front_default)
-        console.log(pokemon)
-    })
+function pull(){
+    const fetchPokemon = (name, img) => {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 
+        fetch(url)
+        .then(response => response.json())
+        .then(pokemon => {
+            name = pokemon.name
+            img = pokemon.sprites.front_default
+            console.log("Nome: "+name)
+            console.log("\nId: "+id)
+            console.log("\nimg: "+ img)
+
+            document.querySelector(".pomodoro__pokebola").innerHTML = `
+            <img src="${pokemon.sprites.front_default}"/>
+            `
+
+            if(rare.includes(id) == true){
+                console.log('Raro')
+            }
+            else if(legendary.includes(id) == true){
+                console.log('Lendario')
+            }
+            else{
+                console.log('Comum')
+            }
+        })
+    }
+    fetchPokemon()
 }
 
+pull()
 
-fetchPokemon()
+console.log(name)
